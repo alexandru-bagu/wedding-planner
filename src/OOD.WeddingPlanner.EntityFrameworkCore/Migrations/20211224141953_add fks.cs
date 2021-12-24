fc88ec41058923e0d8c0a3921b1cb7fe1690a745
Alexandru Bagu <alexandru.bagu@gmail.com>
2021-12-24 16:28:23 +0200

    add foreign keys

---
+++ src/OOD.WeddingPlanner.EntityFrameworkCore/Migrations/20211224141953_add fks.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OOD.WeddingPlanner.Migrations
+{
+    public partial class addfks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AppInvitees_InvitationId",
+                table: "AppInvitees",
+                column: "InvitationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppInvitations_WeddingId",
+                table: "AppInvitations",
+                column: "WeddingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppEvents_LocationId",
+                table: "AppEvents",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppEvents_WeddingId",
+                table: "AppEvents",
+                column: "WeddingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppEvents_AppLocations_LocationId",
+                table: "AppEvents",
+                column: "LocationId",
+                principalTable: "AppLocations",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppEvents_AppWeddings_WeddingId",
+                table: "AppEvents",
+                column: "WeddingId",
+                principalTable: "AppWeddings",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppInvitations_AppWeddings_WeddingId",
+                table: "AppInvitations",
+                column: "WeddingId",
+                principalTable: "AppWeddings",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppInvitees_AppInvitations_InvitationId",
+                table: "AppInvitees",
+                column: "InvitationId",
+                principalTable: "AppInvitations",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppEvents_AppLocations_LocationId",
+                table: "AppEvents");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppEvents_AppWeddings_WeddingId",
+                table: "AppEvents");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppInvitations_AppWeddings_WeddingId",
+                table: "AppInvitations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppInvitees_AppInvitations_InvitationId",
+                table: "AppInvitees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AppInvitees_InvitationId",
+                table: "AppInvitees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AppInvitations_WeddingId",
+                table: "AppInvitations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AppEvents_LocationId",
+                table: "AppEvents");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AppEvents_WeddingId",
+                table: "AppEvents");
+        }
+    }
+}
