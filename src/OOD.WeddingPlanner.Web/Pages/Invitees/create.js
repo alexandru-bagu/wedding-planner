535d7082f408610fb3c11c9a50d83d1f03d9386c
Alexandru Bagu <alexandru.bagu@gmail.com>
2022-02-05 10:15:01 +0200

    fix create.js

--- src/OOD.WeddingPlanner.Web/Pages/Invitees/create.js
+++ src/OOD.WeddingPlanner.Web/Pages/Invitees/create.js
@@ -2,7 +2,8 @@
 
 abp.modals.inviteeCreateModal = function () {
     var initModal = function (publicApi, args) {
-
+        if(publicApi.__init__) return;
+        publicApi.__init__ = true;
         publicApi.onOpen(function () {
             var modal = publicApi.getModal();
             modal.find('select').select2();
