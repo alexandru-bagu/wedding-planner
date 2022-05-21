8f4f04123e2fc3bd9832a53dd8853f99b3013c76
Alexandru Bagu <alexandru.bagu@gmail.com>
2022-05-21 16:42:01 +0300

    add table menu entity

---
+++ src/OOD.WeddingPlanner.Domain/TableMenus/TableMenu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace OOD.WeddingPlanner.TableMenus
+{
+    public class TableMenu : FullAuditedEntity<Guid>, IMultiTenant
+    {
+        public virtual Guid? TenantId { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual bool Adult { get; set; }
+
+        public virtual int Order { get; set; }
+
+        protected TableMenu()
+        {
+        }
+    }
+}
