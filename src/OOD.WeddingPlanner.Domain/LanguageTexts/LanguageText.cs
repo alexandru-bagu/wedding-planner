b1fae93e83ee0eccc7a85f8214aaf5dcdb13b615
Alexandru Bagu <alexandru.bagu@gmail.com>
2022-05-21 23:35:59 +0300

    add language text entity

---
+++ src/OOD.WeddingPlanner.Domain/LanguageTexts/LanguageText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace OOD.WeddingPlanner.LanguageTexts
+{
+    public class LanguageText : FullAuditedEntity<Guid>, IMultiTenant
+    {
+        public Guid? TenantId { get; set; }
+        public string ResourceName { get; set; }
+        public string CultureName { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
